5377f7fe964f50913e10ff873cc245522ff2df10
Elena Koroleva <koroliana@yandex.ru>
2022-08-24 09:55:03 +0700

    creation of alphabet

--- src/main/java/ru/javarush/cryptoanaliser/koroleva/constants/Strings.java
+++ src/main/java/ru/javarush/cryptoanaliser/koroleva/constants/Strings.java
@@ -1,10 +1,14 @@
 package ru.javarush.cryptoanaliser.koroleva.constants;
 
 public class Strings {
-    private static final String rus = "абвгдеёжзиклмнопрстуфхцчшЪьэюя";
-    private static final String en = "abcdeijklmnoprstyz";
+    private static final String rus = "абвгдеёжзийклмнопрстуфхцчшщъыьэюя";
     private static final String numbers = "1234567890";
-    private static final String symbols = ",._;:-";
-    public static String ALPHABET = rus + rus.toUpperCase() + en + en.toUpperCase() + numbers + symbols;
+    private static final String symbols = ".,-_—«»\";' \\:!?*/";
+    //private static final char[] en = {'a', 'b', 'c', 'd'};
+    private static final String alphabet = rus + rus.toUpperCase() + numbers + symbols;
+    public static final char[] ALPHABET = alphabet.toCharArray();
+    //public static final String ALPHABET = rus + rus.toUpperCase() + numbers + symbols;
 
 }
+
+
